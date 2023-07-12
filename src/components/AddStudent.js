6474f432462a244c0c7c360bbef725e41547ed42
AngelaCheng <acheng@csumb.edu>
2023-07-11 22:03:40 -0700

    implement feature add student

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -7,7 +7,71 @@ import TextField from '@mui/material/TextField';
 
 // properties AddStudent is required, function called when Add clicked.
 class AddStudent extends Component {
-	
+	constructor(props){
+        super(props);
+        this.state=({student_email:'', student_name:'', student_id: 0, status_code:0});
+    }
+    
+    handleSubmit = () =>{
+        fetch('http://localhost:8080/student/',
+        {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            id: this.state.student_id,
+            student_email: this.state.student_email,
+            student_name: this.state.student_name,
+            status_code: this.state.status_code     
+        })
+        }).then(res => {
+            if (res.ok) {
+              toast.success("Student Succesfully Registered", {
+                  position: toast.POSITION.BOTTOM_LEFT
+              });
+            } else {
+              toast.error("Student Registreation Failed", {
+                  position: toast.POSITION.BOTTOM_LEFT
+              });
+              console.error('Post http status =' + res.status);
+        }}).catch(err => {
+            console.error(err);
+          }) 
+    }
+    
+    handleChange = (event) =>  {
+        this.setState({[event.target.name]: event.target.value});
+    }
+    
+    render()  {
+		const {student_email, student_name, student_id, status_code} = this.state;
+        return (
+			<div>
+	            <b>Register Student</b>
+	            
+	            <br/>
+	            <br/>
+	            
+	            <TextField autoFocus style = {{width:200}}
+	                label="Student Name" name="student_name"
+	                onChange={this.handleChange} value={student_name} />
+	            
+	            <br/>
+	            <br/>
+	            
+	            <TextField autoFocus style = {{width:200}}
+	                label="Student Email" name="student_email"
+	                onChange={this.handleChange} value={student_email} />
+	            
+	            <br/> 
+	            <br/>
+	            
+	            <Button variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleSubmit}>Submit</Button>
+	            <ToastContainer autoClose={1500} />   
+             </div>
+        );
+    }
 }
 
 
